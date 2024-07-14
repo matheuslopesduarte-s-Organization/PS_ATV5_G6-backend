66f97ce58b036d98db72a27fc4505c6d65d40e3c
matheuslopesduarte <matheuslopesduarte7@gmail.com>
2024-07-14 19:06:02 -0300

    feat: mail Provider (zoho mail, dominio pessoal) para o cadastro #3

---
+++ app/Mail/Email.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Email extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($details, $subject, $view)
+    {
+        $this->details = $details;
+        $this->subject = $subject;
+        $this->view = $view;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject($this->subject)
+                    ->view($this->view)
+                    ->with('details', $this->details);
+    }
+}

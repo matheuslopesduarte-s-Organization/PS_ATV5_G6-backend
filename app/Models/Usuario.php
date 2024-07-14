729804fbae4c0c6c21554d8015a5aab6824c6fee
matheuslopesduarte <matheuslopesduarte7@gmail.com>
2024-07-14 18:52:10 -0300

    feat: Models PROVISORIAS #2

---
+++ app/Models/Usuario.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id',
+        'nome',
+        'email',
+        'senha',
+        'cpf',
+        'username',
+        'misc',
+        'birthdate'
+    ];
+
+    protected $hidden = [
+        'senha',
+    ];
+    protected $casts = [
+        'misc' => 'array',
+    ];
+
+    public $timestamps = false;
+}
+
